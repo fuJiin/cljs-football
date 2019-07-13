4b25de6949437c623c39e73bd71345d3e5606ce4
Elpizo Choi <elpizo@harbor.com>
2019-07-12 19:16:49 -0700

    Replace canvas with CLJS

--- src/cljs_football/core.cljs
+++ src/cljs_football/core.cljs
@@ -1,5 +1,19 @@
 (ns cljs-football.core
-  (:require [footballsimulationengine :as engine]))
+  (:require [reagent.core :as r]
+            [footballsimulationengine :as engine]))
 
-(defn main []
-  (println "hello"))
+(defn app []
+  [:canvas
+   {:id "map"
+    :style {:border "1px solid #000000"
+            :background "url('img/pitch.jpg')"
+            :background-size "100% 100%"}}])
+
+(defn stop []
+  (println "Stopping..."))
+
+(defn start []
+  (println "Starting...")
+  (r/render
+   [app]
+   (.getElementById js/document "app")))
